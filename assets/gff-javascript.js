a92cb7fcd114dc3807e063683ec1817c0eb6504a
Steve King <steve@mydev.co>
2024-09-23 18:05:26 +0300

    Use className for simplifying mobile/desktop CSS

--- assets/gff-javascript.js
+++ assets/gff-javascript.js
@@ -1,7 +1,21 @@
 /** /assets/gff-javascript.js */
 (() => {
 
-  function onDomContentLoaded() {}
+  function onDomContentLoaded() {
+    monitorDeviceSize();
+  }
+
+  function onDeviceSizeChange({matches = false}) {
+    document.documentElement.classList.toggle('gff-desktop', !matches);
+    document.documentElement.classList.toggle('gff-mobile', matches);
+  }
+
+  function monitorDeviceSize() {
+    const result = matchMedia('(max-width: 768px)');
+    onDeviceSizeChange(result);
+
+    result.addEventListener('change', onDeviceSizeChange);
+  }
 
   document.addEventListener('DOMContentLoaded', onDomContentLoaded);
 
